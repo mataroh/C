c56789b9dd158d66d5ab98d7358d68c5118a4a5c
mataroh <henriquemataro123@gmail.com>
2023-03-01 14:58:27 -0300

    Anotações

--- CursoYtt/Hello/primeiro.c
+++ CursoYtt/Hello/primeiro.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* COMENTÁRIOS
+/*
+
+AULA 1:
+
 printf - Função que imprime algo na tela.
  /n - SubCódigo para pular linha
 <stdlib.h> - chama a função "system ("pause")" para parar o programa
 
-Sintaxe é a rega, como escrever
+AULL 2:
 
+Sintaxe é a rega, como escrever
+Semantica é a lógica
 */
 
 int main () {
