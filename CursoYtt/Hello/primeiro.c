2a3c6abf045b83aad6eebf3b85bb6862dc4b4615
mataroh <henriquemataro123@gmail.com>
2023-03-01 14:07:06 -0300

    Arrumando Diretórios

--- CursoYtt/Hello/primeiro.c
+++ CursoYtt/Hello/primeiro.c
@@ -5,6 +5,9 @@
 printf - Função que imprime algo na tela.
  /n - SubCódigo para pular linha
 <stdlib.h> - chama a função "system ("pause")" para parar o programa
+
+Sintaxe é a rega, como escrever
+
 */
 
 int main () {
